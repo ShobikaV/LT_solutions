1a9452a188e38afa9b2ae1aa6b2d756f48265cba
ShobikaV_02 <84139514+ShobikaV@users.noreply.github.com>
2022-03-31 23:19:30 +0530

    Time: 32 ms (95.32%), Space: 13.8 MB (98.61%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        for char in t:
+            if s.count(char)!=t.count(char):
+                return char
+                break
