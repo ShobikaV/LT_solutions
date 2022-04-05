6094efe86d5c430aec675980699ea0a417586630
ShobikaV_02 <84139514+ShobikaV@users.noreply.github.com>
2022-04-06 00:12:03 +0530

    Time: 169 ms (9.86%), Space: 13.8 MB (56.52%) - LeetHub

--- 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.py
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.py
@@ -3,10 +3,7 @@ class Solution:
         a=''
         for i in range(len(nums)):
             if i%10==nums[i]:
-                a=i 
-                break
-        if a=='':
-            return -1
-        else:
-            return a
+                return i 
+        return -1
+     
             
