77445d9f97962410f56bc410e6fdb373e5e54107
ShobikaV_02 <84139514+ShobikaV@users.noreply.github.com>
2022-04-17 11:22:15 +0530

    Time: 43 ms (95.65%), Space: 14.2 MB (55.06%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        flag=0
+        for i in ransomNote:
+            if i in magazine:
+                ransomNote=ransomNote.replace(i,"",1)
+                magazine=magazine.replace(i,"",1)
+        if len(ransomNote)==0:
+            return True
+        else:
+            return False
+        
