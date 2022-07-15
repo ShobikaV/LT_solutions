79c6d8c10270d472ad7474d5205cd2fd0557e8fc
ShobikaV_02 <84139514+ShobikaV@users.noreply.github.com>
2022-07-15 09:11:55 +0530

    Added solution - LeetHub

---
+++ Subarray with 0 sum - GFG/subarray-with-0-sum.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check whether there is a subarray present with 0-sum or not.
+    def subArrayExists(self,arr,n):
+        subsum=0
+        s=set()
+        for i in arr:
+            subsum+=i
+            if subsum==0 or subsum in s:
+                return True
+            s.add(subsum)
+        return False
+            
+            
+            
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+
+def main():
+    T=int(input())
+    while(T>0):
+        
+        n=int(input())
+        arr=[int(x) for x in input().strip().split()]
+        if(Solution().subArrayExists(arr,n)):
+            print("Yes")
+        else:
+            print("No")
+        
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
