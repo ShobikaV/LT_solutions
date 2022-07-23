513c17220497853215676f05c5747a351cfc2b8e
ShobikaV_02 <84139514+ShobikaV@users.noreply.github.com>
2022-07-23 08:49:57 +0530

    Time: 55 ms (29.52%), Space: 13.9 MB (17.88%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,20 +1,18 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         matrix=[]
-        if numRows<3:
-            for i in range(numRows):
-                matrix.append([1]*(i+1))
-        else:
-            prev_row=[1,1]
+        if numRows==1:
             matrix.append([1])
-            matrix.append(prev_row)
-            for i in range(2,numRows):
-                row=[0]*(i+1)
+        else:
+            matrix.append([1])
+            prev_row=[]
+            for i in range(1,numRows):
+                row=[]
                 for j in range(i+1):
                     if j==0 or j==i:
-                        row[j]=1
+                        row.append(1)
                     else:
-                        row[j]=prev_row[j-1]+prev_row[j]
+                        row.append(prev_row[j-1]+prev_row[j])
                 prev_row=row
                 matrix.append(row)
         return(matrix)
